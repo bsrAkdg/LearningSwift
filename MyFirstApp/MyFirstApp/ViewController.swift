f390e5b92cd6e8511aea4c7bb1644dd2bd3a376f
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-09 15:57:48 +0300

    imageView and button trying added

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClick(_ sender: Any) {
+        imageView.image = UIImage(named : "metallica2")
+    }
 }
 
