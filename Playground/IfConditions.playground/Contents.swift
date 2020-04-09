456924b935859c55a6f75fd89c356bcb6e35501f
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-09 21:41:10 +0300

    if else

---
+++ Playground/IfConditions.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var myAge = 27
+
+if myAge < 60 {
+    print("You are young :) ")
+} else if myAge > 60 && myAge < 90 {
+    print("You are even younger :) ")
+} else {
+    print("Whether your soul is young :) ")
+}
+
+var myString = "Ayse"
+
+if myString == "Ayse" {
+    print("You are Ayse")
+}
+
+if myString == "Fatma" {
+    print("You are Fatma")
+} else {
+    print("You aren't Fatma")
+}
