4d4d34c79c2057c339d1b9939e477bc25d6797ed
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-09 18:37:06 +0300

    Set

--- Playground/Arrays.playground/Contents.swift
+++ Playground/Arrays.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// ARRAY
+
 var myFavoriteMovies = ["Pulp Fiction", "The Godfather", "Forrest Gump"]
 
 // as -> casting
@@ -22,6 +24,25 @@ myFavoriteMovies.last
 
 var myNumbers = [1, 2, 3, 4, 5, 6] // integer
 
+// SET : each item is unique and items are unordered
+
+var mySet : Set = [1, 2, 3, 4, 5, 2, 3, 4]
+print(mySet)
+
+mySet.first // unordered
+var myStringSet : Set = ["a", "b", "c", "d", "a", "b"]
+
+var myDefaultArray = [1, 2, 3, 5, 4, 2, 3, 5]
+var myNewSet = Set(myDefaultArray)
+
+print("My default array :")
+print(myDefaultArray)
+
+print("My new set from my default array :")
+print(myNewSet)
 
 
-
+var mySet1 : Set = [1, 2, 3]
+var mySet2 : Set = [3, 4, 5]
+var mySet3 = mySet1.union(mySet2)
+print(mySet3)
