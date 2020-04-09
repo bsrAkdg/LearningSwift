0e9bc8261872b759f3d578a94618367d76f04e20
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-09 17:19:41 +0300

    Variables

---
+++ Playground/Variables.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+// Variables & Consttants
+// var : mutable
+// let : immutable
+
+// snake case naming:
+var user_nick = "Ayse"
+
+// camelCase naming:
+var userNick = "Ayse"
+
+// Variable Types : String, integer, double, float
+
+// String
+var userName = "Ayse"
+var userSurname = "Çetin"
+
+userName.append("l")
+print(userName)
+userName.uppercased()
+print(userName)
+
+print(userSurname)
+userSurname = "Yılmaz"
+print(userSurname)
+
+let userAge = 50
+
+// userAge = 30 error : userAge immutable
+
+let pi = 3.14
+
+// userAge * pi error : different types
+
+var radius = 2.5
+var field = pi * radius * radius
+print(field)
+
+// integer
+var userCode = 50
+var myNumber = 4
+print(userCode/myNumber) // result is Integer
+
+// double
+var userCodeD = 50.0
+var myNumberD = 4.0
+print(userCodeD/myNumberD) // result is Double
+
+// boolean
+var myBoolean = true
+myBoolean = false
