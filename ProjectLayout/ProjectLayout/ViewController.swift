6d72d5784032710296d1a0af70f700ad0872efb7
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-10 20:42:41 +0300

    layout and constraint

---
+++ ProjectLayout/ProjectLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Büşra Serdaroğlu on 10.04.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
