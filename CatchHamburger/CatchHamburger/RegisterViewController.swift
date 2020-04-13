825bdc39b6dfe880431b06ffa2618cf507ceee94
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-13 22:17:14 +0300

    Register control add

--- CatchHamburger/CatchHamburger/RegisterViewController.swift
+++ CatchHamburger/CatchHamburger/RegisterViewController.swift
@@ -11,9 +11,7 @@ import UIKit
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var textFieldUsername: UITextField!
-    
     @IBOutlet weak var textFieldPassword: UITextField!
-    
     @IBOutlet weak var textFieldPasswordAgain: UITextField!
     
     override func viewDidLoad() {
@@ -22,6 +20,30 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func register(_ sender: Any) {
+        let username = textFieldUsername.text
+        let password = textFieldPassword.text
+        let passwordAgain = textFieldPasswordAgain.text
         
+        if username == nil {
+            showAlert(title: "Error", message: "Please enter username")
+        } else if password == nil {
+            showAlert(title: "Error", message: "Please enter password")
+        } else if passwordAgain == nil {
+            showAlert(title: "Error", message: "Please enter password again")
+        } else if password != passwordAgain {
+            showAlert(title: "Error", message: "Passwords not matched")
+        } else {
+            UserDefaults.standard.set(username, forKey: "username")
+            UserDefaults.standard.set(0, forKey: "score")
+            
+            showAlert(title: "Success", message: "You are saved")
+            
+            performSegue(withIdentifier: "loginPage", sender: nil)
+        }
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController.init(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        self.present(alert, animated: true, completion: nil)
     }
 }
