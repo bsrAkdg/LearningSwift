6af395e879b51f40e9a4fd7ad48d5e9aafe54da5
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-15 13:51:46 +0300

    Class add

---
+++ MusicianClass/MusicianClass/Musicians.swift
@@ -0,0 +1,22 @@
+//
+//  Musicians.swift
+//  MusicianClass
+//
+//  Created by Büşra Serdaroğlu on 15.04.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import Foundation
+
+class Musicians {
+    
+    // Property
+    var name : String = ""
+    var age : Int = 0
+    var instrument : String = ""
+
+    // Initializer (Constructor)
+    init() {
+        print("Musician created")
+    }
+}
---
+++ MusicianClass/MusicianClass/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  MusicianClass
+//
+//  Created by Büşra Serdaroğlu on 15.04.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import Foundation
+
+let james = Musicians()
+
+//james.age = 50
+//james.name = "James Hetfield"
+//james.instrument = "Guitar"
+
+print(james.name)
+
+
