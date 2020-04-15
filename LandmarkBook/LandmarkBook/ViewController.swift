71fc0ddda601c06c87e8e54e496d255a5ffa410d
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-15 11:17:06 +0300

    tableview initial

---
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Büşra Serdaroğlu on 14.04.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+}
+
