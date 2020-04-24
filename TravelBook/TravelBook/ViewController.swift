ef7ef63fe3befe8baa54de72d897f56e1c210285
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-25 01:40:21 +0300

    notification center add

--- TravelBook/TravelBook/ViewController.swift
+++ TravelBook/TravelBook/ViewController.swift
@@ -171,6 +171,10 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         } catch {
             print("error")
         }
+        
+        // notfiy
+        NotificationCenter.default.post(name: NSNotification.Name("newPlace"), object: nil)
+        navigationController?.popViewController(animated: true)
     }
 }
 
