912dcd2411b09d705f63321cb5a53ada096503da
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-28 02:09:56 +0300

    Simpson class fixed

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  SimpsonBook
+//
+//  Created by Büşra Serdaroğlu on 17.04.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+}
