a6f1752b63234aa8d77c723a00ce8bf119d05510
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-28 00:14:09 +0300

    Dark mode and info.plist control

--- DarkModeProject/DarkModeProject/ViewController.swift
+++ DarkModeProject/DarkModeProject/ViewController.swift
@@ -15,7 +15,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // User light mode in this controller
+        overrideUserInterfaceStyle = .light
+        
+        // if you add User Interface Style to info.plist Dark or Light : Enforce!
     }
 
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        // get user ui mode with traitCollection
+        // viewWillAppear and viewDidLoad dont update when app open, we should use traitCollectionDidChange
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            buttonChange.tintColor = UIColor.white
+        } else {
+            buttonChange.tintColor = UIColor.black
+        }
+    }
 }
 
