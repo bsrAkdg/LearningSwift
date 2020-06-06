51aa4f86a61bf068476243ac5f8e570e03304b4e
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-06 23:03:04 +0300

    Reference vs Value types

--- AdvancedSwift/AdvancedSwift/main.swift
+++ AdvancedSwift/AdvancedSwift/main.swift
@@ -10,6 +10,8 @@ import Foundation
 
 print("Hello, World!")
 
+// CLASS vs STRUCT
+
 // you must create init method on musician class
 let musicianClassInstance = MusicianClass(nameInput: "Okan", ageInput: 32, instrumentInput: "Guitar")
 print(musicianClassInstance.name)
@@ -17,3 +19,38 @@ print(musicianClassInstance.name)
 // free initializer
 let musicianStructInstance = MusicianStruct(name: "Okan", age: 32, instrument: "Guitar")
 print(musicianStructInstance.name)
+
+musicianClassInstance.age = 33 // age change!
+print(musicianClassInstance.age)
+
+// musicianStructInstance.age = 33 // you dont this, structs are true immutability
+
+var musicianNewStructInstance = MusicianStruct(name: "Okan", age: 32, instrument: "Guitar")
+musicianNewStructInstance.age = 33 // age change!
+print(musicianNewStructInstance.age)
+
+
+// REFERENCE vs VALUE
+
+let copyOfClassMusicianClass = musicianClassInstance // reference type
+var copyOfStructMusicianStruct = musicianNewStructInstance // value type
+
+print(copyOfClassMusicianClass.age)
+print(copyOfStructMusicianStruct.age)
+
+copyOfClassMusicianClass.age = 34
+copyOfStructMusicianStruct.age = 34
+
+print(copyOfClassMusicianClass.age)
+print(copyOfStructMusicianStruct.age)
+
+print(musicianClassInstance.age)
+print(musicianNewStructInstance.age)
+
+// Reference types -> Class
+// Copy -> Same object new reference -> 1 object 2 reference
+
+// Value types -> Struct
+// Copy -> New object create -> 2 object
+
+
