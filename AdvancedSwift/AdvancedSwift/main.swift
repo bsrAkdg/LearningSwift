2cb6f60a497b66671dd556069c8fe8fc1dc2b426
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-06 22:37:55 +0300

    initial project

---
+++ AdvancedSwift/AdvancedSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvancedSwift
+//
+//  Created by Büşra Serdaroğlu on 6.06.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
