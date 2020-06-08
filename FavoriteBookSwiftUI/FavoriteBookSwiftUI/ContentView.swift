5ccd5d8d241d1771786070113a5fe5ce92ce0164
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-08 22:20:44 +0300

    Project create

---
+++ FavoriteBookSwiftUI/FavoriteBookSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteBookSwiftUI
+//
+//  Created by Büşra Serdaroğlu on 8.06.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
