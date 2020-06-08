9a67d16a7ccd8fa3acf09e6ba347ed519319817b
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-08 22:53:54 +0300

    Header footer list

--- FavoriteBookSwiftUI/FavoriteBookSwiftUI/FavoriteModel.swift
+++ FavoriteBookSwiftUI/FavoriteBookSwiftUI/FavoriteModel.swift
@@ -27,9 +27,18 @@ struct FavoriteElements : Identifiable {
 }
 
 
+// Bands
 let metallica = FavoriteElements(name: "Metallica", imagename: "metallica", description: "Best music 1!!!!")
 let megadeth = FavoriteElements(name: "Megadeth", imagename: "megadeth", description: "Best music 2!!!!")
 let ironmaiden = FavoriteElements(name: "Ironmaiden", imagename: "ironmaiden", description: "Best music 3!!!!")
 
 let favoriteBand = FavoriteModel(title: "Favorite Bands", elements: [metallica, megadeth, ironmaiden])
 
+// Moview
+let pulpFiction = FavoriteElements(name: "Pulpfiction", imagename: "pulpfiction", description: "No 1 Movie")
+let theDarkNight = FavoriteElements(name: "The Dark Night", imagename: "thedarknight", description: "No 2 Movie")
+let killBill = FavoriteElements(name: "Kill Bill", imagename: "killbill", description: "No 3 Movie")
+
+let favoriteMovie = FavoriteModel(title: "Favorite Movies", elements: [pulpFiction, theDarkNight, killBill])
+
+let myFavorites = [favoriteBand, favoriteMovie]
